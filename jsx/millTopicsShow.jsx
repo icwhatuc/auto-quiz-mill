bedbb24aa084707c1aec9735eb1719b315726e04
Mihir Patel <ee.mihir.patel@gmail.com>
2015-08-03 23:08:25 -0400

    col mode, responsive, less whitespace

--- jsx/millTopicsShow.jsx
+++ jsx/millTopicsShow.jsx
@@ -21,13 +21,25 @@ ctx.stroke();
 var MillTopicsShow = React.createClass({
     render : function() {
         return (
-            <ComponentGallery className="photos" margin="10" widthHeightRatio="3/5" targetWidth="100">
-                <a href="google.com"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/December_2009_partrial_lunar_eclipse-cropped.jpg/300px-December_2009_partrial_lunar_eclipse-cropped.jpg" /></a>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Googleplex-Patio-Aug-2014.JPG/300px-Googleplex-Patio-Aug-2014.JPG" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Derek_Jeter_batting_stance_allison.jpg/200px-Derek_Jeter_batting_stance_allison.jpg" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wetten_dass_20130323_6475.jpg/300px-Wetten_dass_20130323_6475.jpg" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg/300px-Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Violin_VL100.png/221px-Violin_VL100.png" />
+            <ComponentGallery className="photos" margin="5" widthHeightRatio="5/3" targetWidth="300">
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/December_2009_partrial_lunar_eclipse-cropped.jpg/300px-December_2009_partrial_lunar_eclipse-cropped.jpg" />
+                </a>
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Googleplex-Patio-Aug-2014.JPG/300px-Googleplex-Patio-Aug-2014.JPG" />
+                </a>
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Derek_Jeter_batting_stance_allison.jpg/333px-Derek_Jeter_batting_stance_allison.jpg" />
+                </a>
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wetten_dass_20130323_6475.jpg/300px-Wetten_dass_20130323_6475.jpg" />
+                </a>
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg/300px-Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg" />
+                </a>
+                <a href="google.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Violin_VL100.png/221px-Violin_VL100.png" />
+                </a>
             </ComponentGallery>  
         );
     }
