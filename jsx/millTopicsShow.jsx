f3f69198a8787892e915dbbaa143187e7e47b3f5
Mihir Patel <ee.mihir.patel@gmail.com>
2015-08-04 00:46:15 -0400

    new topics display integrated with real data

--- jsx/millTopicsShow.jsx
+++ jsx/millTopicsShow.jsx
@@ -1,45 +1,75 @@
 var ComponentGallery = require('react-component-gallery');
-
-/*
-var Canvas = require('canvas')
-  , Image = Canvas.Image
-  , canvas = new Canvas(300, 300)
-  , ctx = canvas.getContext('2d');
-
-ctx.font = '30px Impact';
-ctx.rotate(.1);
-ctx.fillText("Awesome!", 50, 100);
-
-var te = ctx.measureText('Awesome!');
-ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.lineTo(50 + te.width, 102);
-ctx.stroke();
-*/
+var es = require('elasticsearch');
+var eshost = 'https://mpetgxg8c4:8sl7z9gaui@quiz-9057819005.us-east-1.bonsai.io';
+var esindex = 'auto-quiz-mill';
+var estype = 'entities';
+var esclient = new es.Client({
+    host: eshost
+});
 
 var MillTopicsShow = React.createClass({
+    getInitialState : function() {
+        return { data : [] };
+    },
+    componentDidMount : function() {
+        var self = this;
+        esclient.search({
+            index : esindex,
+            type : estype,
+            body : {
+                _source : ["name", "description", "entity_url", "img_url", "views_last_month", "incoming_links_count"],
+                query : {
+                    filtered : {
+                        filter : {
+                            exists : {
+                                field : "img_url"
+                            }
+                        }
+                    }
+                },
+                sort : [
+                    { views_last_month : { "order" : "desc", "missing" : "_last" } },
+                    { incoming_links_count : { "order" : "desc", "missing" : "_last" } },
+                ]
+            }
+        }, function(error, res) {
+            if(error) {
+                console.error("Unable to retrieve data from server.  Please try again later."); 
+                return;
+            }
+            else
+            {
+                var data = [];
+                res.hits.hits.forEach(function(hit) {
+                    data.push({
+                        name : hit._source.name,
+                        url : hit._source.entity_url,
+                        img_url : hit._source.img_url
+                    });
+                });
+                self.setState({ data : data });
+            }
+        });
+    },
     render : function() {
+        var entityNodes = this.state.data.map(function(entity) {
+            return (
+                <a href={entity.url}>
+                    <img src={entity.img_url}/>
+                </a>
+            );
+        });
+        
+        if(!entityNodes.length)
+        {
+            return (
+                <p>hello, world!</p>
+            );
+        }
+
         return (
             <ComponentGallery className="photos" margin="5" widthHeightRatio="5/3" targetWidth="300">
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/December_2009_partrial_lunar_eclipse-cropped.jpg/300px-December_2009_partrial_lunar_eclipse-cropped.jpg" />
-                </a>
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Googleplex-Patio-Aug-2014.JPG/300px-Googleplex-Patio-Aug-2014.JPG" />
-                </a>
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Derek_Jeter_batting_stance_allison.jpg/333px-Derek_Jeter_batting_stance_allison.jpg" />
-                </a>
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wetten_dass_20130323_6475.jpg/300px-Wetten_dass_20130323_6475.jpg" />
-                </a>
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg/300px-Coldplay_Viva_La_Vida_Tour_in_Hannover_August_25th_2009.jpg" />
-                </a>
-                <a href="google.com">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Violin_VL100.png/221px-Violin_VL100.png" />
-                </a>
+                {entityNodes}
             </ComponentGallery>  
         );
     }
